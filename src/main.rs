4a6ceac9d189846f685ab2ede29b9a429fe49b5a
Matej Almasi <almasi.mato@gmail.com>
2025-05-03 22:26:23 +0200

    update main to reflect latest greates proxy

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-use std::net::SocketAddr;
+use std::env;
 
 use rust_proxy::Proxy;
+use tracing_appender::non_blocking::WorkerGuard;
 
 #[tokio::main]
 async fn main() {
-    let file_appender = tracing_appender::rolling::never("/logs", "proxy.log");
-    let (writer, _guard) = tracing_appender::non_blocking(file_appender);
+    let args = env::args().collect::<Vec<_>>();
+    let host_address = &args[1];
 
-    tracing_subscriber::fmt()
-        .compact()
-        .with_writer(writer)
-        .init();
+    let _guard = setup_tracing_to_log_file();
 
-    let self_address = SocketAddr::from(([127, 0, 0, 1], 5609));
-    let host_address = SocketAddr::from(([127, 0, 0, 1], 3456));
-
-    let _ = Proxy::builder()
-        .bind(self_address, host_address)
+    let proxy = Proxy::builder()
+        .bind("127.0.0.1:0", host_address)
         .await
-        .expect("Failed to build proxy.");
+        .unwrap_or_else(|e| panic!("Failed to create proxy: {e}"));
+
+    let test_address = proxy
+        .local_addr()
+        .unwrap_or_else(|e| panic!("Failed to get own address: {e}"));
+
+    println!("Listening on {test_address}");
+
+    proxy.run().await;
+}
+
+fn setup_tracing_to_log_file() -> WorkerGuard {
+    let file_appender = tracing_appender::rolling::never("./logs", "proxy.log");
+    let (writer, guard) = tracing_appender::non_blocking(file_appender);
+
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_ansi(false)
+        .with_writer(writer)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    guard
 }
